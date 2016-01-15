8d37f9406a4760b45b14fb959a5a2782ca48a967
itzmanojds <itzmanojds@gmail.com>
2016-01-15 17:14:58 +0530

    Initial commit
    Creation of initial workspace

---
+++ CarBookingPortal/Login.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="CarBookingPortal.Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="text-align: center; font-family: Cambria">
+        
+        &nbsp;<br />
+        &nbsp;<br />
+        <br />
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td style="text-align: right">Username :</td>
+                <td style="text-align: left">
+        <asp:TextBox ID="txtBoxUsername" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align: right">Password :</td>
+                <td style="text-align: left">
+        <asp:TextBox ID="txtBoxPassword" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align: right">
+                    <asp:Button ID="btnLogin" runat="server" OnClick="btnLogin_Click" Text="Login" />
+                </td>
+                <td style="text-align: left">
+                    <asp:Button ID="btnReset" runat="server" OnClick="btnReset_Click" Text="Reset" />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CarBookingPortal/Login.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace CarBookingPortal
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string strUsername = txtBoxUsername.Text;//get data entered in txtbox username
+            string strPassword = txtBoxPassword.Text;//get data entered in txtbox password
+
+            MySqlConnection connection = new MySqlConnection("Data Source = Localhost; server = localhost; user = root; pwd = root123; database = db_carbooking; port = 3306;");
+            MySqlCommand command = new MySqlCommand("SELECT COUNT(*) FROM logindetails WHERE username='" + strUsername + "' and password='" + strPassword + "';", connection);
+            connection.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+            int iResult = -1;
+            if (reader.Read())
+            {
+                iResult = reader.GetInt32(0);
+            }
+            connection.Close();
+            if (iResult == 1)//valid user
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('you have logged in successfully');", true);
+                Session["LoginName"] = strUsername;
+                Response.Redirect("~/Home.aspx");
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtBoxUsername.Text = "";
+            txtBoxPassword.Text = "";
+        }
+    }
+}
